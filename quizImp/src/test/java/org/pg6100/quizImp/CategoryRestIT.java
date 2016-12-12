37178aefe46b83f387a22c9063954afcb985bfce
Olsm <OlavOlsm@gmail.com>
2016-12-12 20:04:09 +0100

    improve category rest integration tests

--- quizImp/src/test/java/org/pg6100/quizImp/CategoryRestIT.java
+++ quizImp/src/test/java/org/pg6100/quizImp/CategoryRestIT.java
@@ -1,6 +1,7 @@
 package org.pg6100.quizImp;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 import org.pg6100.quizApi.dto.CategoryDTO;
 import org.pg6100.quizApi.dto.SubCategoryDTO;
@@ -11,57 +12,52 @@ import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.core.Is.is;
 
 public class CategoryRestIT extends CategoryRestTestBase {
+    
+    private static String CATEGORY_PATH = "/categories";
+    private static String SUBCATEGORY_PATH = "/subcategories";
 
     @Test
     public void testCleanDB() {
-
-        get("/categories").then()
-                .statusCode(200)
-                .body("size()", is(0));
+        testGetRootCategories(0);
+        testGetSubCategories(0);
     }
 
     @Test
-    public void testCreateAndGetRootCategory() {
-        get("/categories").then().statusCode(200).body("size()", is(0));
-
-        CategoryDTO dto = createCategoryDTO("name");
-        get("/categories").then().statusCode(200).body("size()", is(1));
-
-        testGetCategory("/categories", dto.id);
+    public void testCreateCategory() {
+        CategoryDTO dto = createCategory("name");
+        testGetRootCategories(1);
+        testGetRootCategory(dto.id);
     }
 
     @Test
-    public void testCreateAndGetSubCategory() {
-        CategoryDTO rootDTO = createCategoryDTO("name");
-
-        get("/subcategories").then().statusCode(200).body("size()", is(0));
-
-        SubCategoryDTO dto = createSubCategoryDTO(rootDTO.id, "name");
-        get("/subcategories").then().statusCode(200).body("size()", is(1));
-        testGetCategory("/subcategories", dto.id);
+    public void testCreateSubCategory() {
+        CategoryDTO rootDTO = createCategory("name");
+        SubCategoryDTO dto = createSubCategory(rootDTO.id, "name");
+        testGetSubCategories(1);
+        testGetSubCategory(dto.id);
     }
 
     @Test
-    public void testGetRootCategories() throws Exception {
-        CategoryDTO dto1 = createCategoryDTO("name1");
-        CategoryDTO dto2 = createCategoryDTO("name2");
-        get("/categories").then().statusCode(200).body("size()", is(2))
-            .body("name", hasItems("name1", "name2"));
+    public void testCreateTwoCategories() throws Exception {
+        createCategory("name1");
+        createCategory("name2");
+        testGetRootCategories(2)
+                .body("name", hasItems("name1", "name2"));
     }
 
     @Test
     public void testUpdateRootCategory() throws Exception {
-        CategoryDTO dto = createCategoryDTO("name1");
+        CategoryDTO dto = createCategory("name1");
         dto.name = "name2";
-        updateCategory(dto, dto.id, "/categories");
-        testGetCategory("/categories", dto.id);
+        updateRootCategory(dto, dto.id);
+        testGetRootCategory(dto.id);
     }
 
     @Test
     public void testDeleteRootCategory() throws Exception {
-        CategoryDTO dto = createCategoryDTO("name");
-        deleteCategory(dto.id, "/categories");
-        get("/categories").then().statusCode(200).body("size()", is(0));
+        CategoryDTO dto = createCategory("name");
+        deleteRootCategory(dto.id);
+        get(CATEGORY_PATH).then().statusCode(200).body("size()", is(0));
     }
 
     /* TODO
@@ -74,37 +70,39 @@ public class CategoryRestIT extends CategoryRestTestBase {
 
     @Test
     public void testGetSubCategories() throws Exception {
-        CategoryDTO dto = createCategoryDTO("name");
-        SubCategoryDTO dto1 = createSubCategoryDTO(dto.id, "name1");
-        SubCategoryDTO dto2 = createSubCategoryDTO(dto.id, "name2");
-        get("/subcategories").then().statusCode(200).body("size()", is(2))
-                .body("name", hasItems("name1", "name2"));
+        CategoryDTO rootDTO1 = createCategory("root1");
+        CategoryDTO rootDTO2 = createCategory("root2");
+        createSubCategory(rootDTO1.id, "sub1");
+        createSubCategory(rootDTO2.id, "sub2");
+        createSubCategory(rootDTO2.id, "sub3");
+        testGetSubCategories(3)
+                .body("name", hasItems("sub1", "sub2", "sub3"));
     }
 
     @Test
     public void testUpdateSubCategory() throws Exception {
-        CategoryDTO rootDTO = createCategoryDTO("root");
-        SubCategoryDTO subDTO = createSubCategoryDTO(rootDTO.id, "name1");
+        CategoryDTO rootDTO = createCategory("root");
+        SubCategoryDTO subDTO = createSubCategory(rootDTO.id, "name1");
         subDTO.name = "name2";
-        updateCategory(subDTO, subDTO.id, "/subcategories");
-        testGetCategory("/subcategories", subDTO.id);
+        updateSubCategory(subDTO, subDTO.id);
+        testGetSubCategory(subDTO.id);
     }
 
     @Test
     public void testDeleteSubCategory() throws Exception {
-        CategoryDTO dto = createCategoryDTO("name");
-        SubCategoryDTO subDTO = createSubCategoryDTO(dto.id, "name");
-        deleteCategory(subDTO.id, "/subcategories");
-        get("/subcategories").then().statusCode(200).body("size()", is(0));
+        CategoryDTO dto = createCategory("name");
+        SubCategoryDTO subDTO = createSubCategory(dto.id, "name");
+        deleteSubCategory(subDTO.id);
+        testGetSubCategories(0);
     }
 
     @Test
     public void testGetSubCategoriesByRootCategory() throws Exception {
-        CategoryDTO rootDTO = createCategoryDTO("root");
-        CategoryDTO rootDTO2 = createCategoryDTO("root2");
-        SubCategoryDTO subDTO = createSubCategoryDTO(rootDTO.id, "sub1");
-        SubCategoryDTO subDTO2 = createSubCategoryDTO(rootDTO.id, "sub2");
-        SubCategoryDTO subDTO3 = createSubCategoryDTO(rootDTO2.id, "sub3");
+        CategoryDTO rootDTO = createCategory("root");
+        CategoryDTO rootDTO2 = createCategory("root2");
+        SubCategoryDTO subDTO = createSubCategory(rootDTO.id, "sub1");
+        SubCategoryDTO subDTO2 = createSubCategory(rootDTO.id, "sub2");
+        SubCategoryDTO subDTO3 = createSubCategory(rootDTO2.id, "sub3");
 
         given().pathParam("id", rootDTO.id)
                 .get("/categories/{id}/subcategories")
@@ -115,11 +113,11 @@ public class CategoryRestIT extends CategoryRestTestBase {
 
     @Test
     public void testGetSubWithGivenParentByCategory() throws Exception {
-        CategoryDTO rootDTO = createCategoryDTO("root");
-        createSubCategoryDTO(rootDTO.id, "sub1");
-        createSubCategoryDTO(rootDTO.id, "sub2");
-        CategoryDTO rootDTO2 = createCategoryDTO("root2");
-        createSubCategoryDTO(rootDTO2.id, "sub3");
+        CategoryDTO rootDTO = createCategory("root");
+        createSubCategory(rootDTO.id, "sub1");
+        createSubCategory(rootDTO.id, "sub2");
+        CategoryDTO rootDTO2 = createCategory("root2");
+        createSubCategory(rootDTO2.id, "sub3");
 
         given().pathParam("id", rootDTO.id)
                 .get("/subcategories/parent/{id}")
@@ -129,16 +127,24 @@ public class CategoryRestIT extends CategoryRestTestBase {
     }
 
 
-    private CategoryDTO createCategoryDTO(String name) {
+    private CategoryDTO createCategory(String name) {
         CategoryDTO dto = new CategoryDTO(name);
-        dto.id = registerCategory(dto, "/categories");
+        dto.id = registerCategory(dto, CATEGORY_PATH);
         return dto;
     }
 
-    private SubCategoryDTO createSubCategoryDTO(String rootCategoryId, String name) {
+    private SubCategoryDTO createSubCategory(String rootCategoryId, String name) {
         SubCategoryDTO dto = new SubCategoryDTO(rootCategoryId, name);
-        dto.id = registerCategory(dto, "/subcategories");
+        dto.id = registerCategory(dto, SUBCATEGORY_PATH);
         return dto;
+    }
+    
+    private void testGetRootCategory(String id) {
+        testGetCategory(CATEGORY_PATH, id);
+    }
+
+    private void testGetSubCategory(String id) {
+        testGetCategory(SUBCATEGORY_PATH, id);
     }
 
     private void testGetCategory(String path, String id) {
@@ -149,6 +155,17 @@ public class CategoryRestIT extends CategoryRestTestBase {
                 .body("id", is(id));
     }
 
+    private ValidatableResponse testGetRootCategories(int size) {
+        return testGetCategories(CATEGORY_PATH, size);
+    }
+
+    private ValidatableResponse testGetSubCategories(int size) {
+        return testGetCategories(SUBCATEGORY_PATH, size);
+    }
+
+    private ValidatableResponse testGetCategories(String path, int size) {
+        return get(path).then().statusCode(200).body("size()", is(size));
+    }
 
     private String registerCategory(Object dto, String path) {
         return given().contentType(ContentType.JSON)
@@ -157,6 +174,14 @@ public class CategoryRestIT extends CategoryRestTestBase {
                 .then()
                 .statusCode(200)
                 .extract().asString();
+    }
+    
+    private void updateRootCategory(Object dto, String id) {
+        updateCategory(dto, id, CATEGORY_PATH);
+    }
+
+    private void updateSubCategory(Object dto, String id) {
+        updateCategory(dto, id, SUBCATEGORY_PATH);
     }
 
     private void updateCategory(Object dto, String id, String path) {
@@ -167,7 +192,15 @@ public class CategoryRestIT extends CategoryRestTestBase {
                 .then()
                 .statusCode(204);
     }
+    
+    private void deleteRootCategory(String id) {
+        deleteCategory(id, CATEGORY_PATH);
+    }
 
+    private void deleteSubCategory(String id) {
+        deleteCategory(id, SUBCATEGORY_PATH);
+    }
+    
     private void deleteCategory(String id, String path) {
         given().contentType(ContentType.JSON)
                 .pathParam("id", id)
