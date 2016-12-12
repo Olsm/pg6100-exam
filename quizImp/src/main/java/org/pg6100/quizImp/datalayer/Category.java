49049f622854dd6169b797640dd90daf4728d34b
Olsm <OlavOlsm@gmail.com>
2016-12-12 13:21:03 +0100

    add entity for Category

---
+++ quizImp/src/main/java/org/pg6100/quizImp/datalayer/Category.java
@@ -0,0 +1,51 @@
+package org.pg6100.quizImp.datalayer;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    @OneToMany(fetch = FetchType.EAGER)
+    private Set<SubCategory> subCategoryList;
+
+    public Category() {}
+
+    public Category(String name) {
+        this.name = name;
+        this.subCategoryList = new HashSet<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String category) {
+        this.name = category;
+    }
+
+    public Set<SubCategory> getSubCategoryList() {
+        return subCategoryList;
+    }
+
+    public void setSubCategoryList(Set<SubCategory> subCategoryList) {
+        this.subCategoryList = subCategoryList;
+    }
+
+    public void addSubCategory(SubCategory category) {
+        getSubCategoryList().add(category);
+    }
+
+    public void removeSubCategory(SubCategory category) {
+        getSubCategoryList().remove(category);
+    }
+}
