d0d2af9b0ac672c1378142f75343b005b3033d70
Olsm <OlavOlsm@gmail.com>
2016-12-13 21:05:37 +0100

    add hystrix configuration

--- gameRest/src/main/java/org/pg6100/gameRest/GameApplication.java
+++ gameRest/src/main/java/org/pg6100/gameRest/GameApplication.java
@@ -1,11 +1,13 @@
 package org.pg6100.gameRest;
 
+import com.netflix.config.ConfigurationManager;
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.jaxrs.listing.ApiListingResource;
+import org.apache.commons.configuration.AbstractConfiguration;
 import org.pg6100.gameRest.api.GameRestImpl;
 
 /*
@@ -54,6 +56,16 @@ public class GameApplication extends Application<GameConfiguration> {
         beanConfig.setBasePath("/game");
         beanConfig.setResourcePackage("org.pg6100.GameAPI");
         beanConfig.setScan(true);
+
+        //Hystrix configuration
+        AbstractConfiguration conf = ConfigurationManager.getConfigInstance();
+        // how long to wait before giving up a request?
+        conf.setProperty("hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds", 500); //default is 1000
+        // how many failures before activating the CB?
+        conf.setProperty("hystrix.command.default.circuitBreaker.requestVolumeThreshold", 2); //default 20
+        conf.setProperty("hystrix.command.default.circuitBreaker.errorThresholdPercentage", 50);
+        //for how long should the CB stop requests? after this, 1 single request will try to check if remote server is ok
+        conf.setProperty("hystrix.command.default.circuitBreaker.sleepWindowInMilliseconds", 5000);
 
         //add further configuration to activate SWAGGER
         environment.jersey().register(new io.swagger.jaxrs.listing.ApiListingResource());
